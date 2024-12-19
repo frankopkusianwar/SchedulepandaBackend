620a8e0f5efca35cac977528efef0f96326b5e8b
okiror frank <okirorfrank3@gmail.com>
2024-12-19 16:45:12 +0300

    fix deploy

--- dist/graphql/resolvers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolvers = void 0;
-const merge_1 = require("@graphql-tools/merge");
-const post_resolvers_1 = require("../services/postService/post.resolvers");
-exports.resolvers = (0, merge_1.mergeResolvers)([
-    post_resolvers_1.postResolvers
-]);
